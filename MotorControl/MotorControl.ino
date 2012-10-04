c8ab0e52d9f2698f2bbf9393d45b3b571993dd3f
Nick Depinet <depinetnick@gmail.com>
2012-10-03 22:55:28 -0400

    Motor Control
    Control the Motors over USB

--- MotorControl/MotorControl.ino
+++ MotorControl/MotorControl.ino
@@ -1,12 +1,10 @@
 
 #include <Sabertooth.h>
-#include <SoftwareSerial.h>
 
 #define rxPin 2
 #define txPin 3
 char xbGet[2];
 
-SoftwareSerial xbee = SoftwareSerial(rxPin, txPin);
 Sabertooth ST(128); 
 
 void setup()
@@ -14,7 +12,6 @@ void setup()
   Serial.begin(9600);
   pinMode(rxPin, INPUT);
   pinMode(txPin, OUTPUT);
-  xbee.begin(9600);  
   ST.drive(0); // The Sabertooth won't act on mixed mode packet serial commands until
   ST.turn(0);  // it has received power levels for BOTH throttle and turning, since it
                // mixes the two together to get diff-drive power levels for both motors.
@@ -23,26 +20,28 @@ void setup()
 void loop()
 {
   int power;
-  if (xbee.available() >= 2) {
+  if (Serial.available() >= 2) {
     for (int i = 0; i <= 1; i++) {
-      xbGet[i] = xbee.read();
+      xbGet[i] = Serial.read();
     }
     xbGet[2] = '\0';
+    Serial.print(xbGet[1]);
   }
   while (strcmp(xbGet, "LT")  == 0) {      
     ST.turn(-127);
-    if (xbee.available() >= 2) {
+    if (Serial.available() >= 2) {
     for (int i = 0; i <= 1; i++) {
-      xbGet[i] = xbee.read();
+      xbGet[i] = Serial.read();
+      Serial.print("JEWS");
     }
     xbGet[2] = '\0';
     }
   }
   while (strcmp(xbGet, "RT")  == 0) {      
     ST.turn(127);
-    if (xbee.available() >= 2) {
+    if (Serial.available() >= 2) {
     for (int i = 0; i <= 1; i++) {
-      xbGet[i] = xbee.read();
+      xbGet[i] = Serial.read();
     }
     xbGet[2] = '\0';
     }
@@ -52,18 +51,18 @@ void loop()
   }
   while (strcmp(xbGet, "FG")  == 0) {      
     ST.drive(127);
-    if (xbee.available() >= 2) {
+    if (Serial.available() >= 2) {
     for (int i = 0; i <= 1; i++) {
-      xbGet[i] = xbee.read();
+      xbGet[i] = Serial.read();
     }
     xbGet[2] = '\0';
     }
   }
   if (strcmp(xbGet, "BG")  == 0) {      
     ST.drive(-127);
-    if (xbee.available() >= 2) {
+    if (Serial.available() >= 2) {
     for (int i = 0; i <= 1; i++) {
-      xbGet[i] = xbee.read();
+      xbGet[i] = Serial.read();
     }
     xbGet[2] = '\0';
     }
